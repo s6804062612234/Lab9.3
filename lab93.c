19b84419b0ad0df6cf4c3025946ae5d0df6ff710
Thananon Sananrum <s6804062612234@email.kmutnb.ac.th>
2026-03-09 03:40:40 +0700

    lab93no5.c

--- lab93.c
+++ lab93.c
@@ -1,65 +1,26 @@
 #include <stdio.h>
-#define MAX 50
-int countVowel(char []);
-char allVowel[5][1] = {"A", "E", "I", "O", "U"};
-int aVowel=0, eVowel=0, iVowel=0, oVowel=0, uVowel=0;
+#define MAX 100
+int add(char []);
 
-void main() {
+int main() {
     char text[MAX];
-    int cVowel, i, j;
+    int sum;
     printf("Enter text : ");
     scanf("%s", text);
-    cVowel = countVowel(text);
-    printf("Text : [%s] has %d vowels\n", text, cVowel);
+    sum = add(text);
+    printf("%d", sum);
     
-    for(i=0; i<5; i++) {
-        printf("%c: ", allVowel[i][0]);
-        if(i==0) {
-            printf("%d", aVowel);
-        } else if(i==1) {
-            printf("%d", eVowel);
-        } else if(i==2) {
-            printf("%d", iVowel);
-        } else if(i==3) {
-            printf("%d", oVowel);
-        } else if(i==4) {
-            printf("%d", uVowel);
-        }
-        
-        printf("\n");
-    }
+    return 0;
 }
 
-int countVowel(char t[]) {
-    int i=0, count=0;
-    while (i<MAX && t[i]!='\0') {
-        if (t[i]=='A' || t[i]=='a' || t[i]=='E' || t[i]=='e' || t[i]=='I' || t[i]=='i' || t[i]=='O' || t[i]=='o' || t[i]=='U' || t[i]=='u') {
-        count++;
-            switch(t[i]) {
-                case 'A':
-                case 'a':
-                    aVowel++;
-                    break;
-                case 'E':
-                case 'e':
-                    eVowel++;
-                    break;
-                case 'I':
-                case 'i':
-                    iVowel++;
-                    break;
-                case 'O':
-                case 'o':
-                    oVowel++;
-                    break;
-                case 'U':
-                case 'u':
-                    uVowel++;
-                    break;
-            }
+int add(char t[]) {
+    int total=0, i;
+    
+    for(i=0; t[i]!='\0'; i++) {
+        if (t[i] >= '0' && t[i] <= '9') {
+            total += t[i] - '0';
         }
-        
-        i++;
     }
-    return(count);
+    
+    return total;
 }
