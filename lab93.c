c76697bb673ed6db4dcb6861e42a50b28c0d7a61
Thananon Sananrum <s6804062612234@email.kmutnb.ac.th>
2026-03-09 02:43:26 +0700

    lab93no4.c

--- lab93.c
+++ lab93.c
@@ -1,26 +1,65 @@
 #include <stdio.h>
-#include <string.h>
-char loginData[5][64]={"user1","user2","user3","user4","user5"};
-char passwordData[5][64]={"pass1","pass2","pass3","pass4","pass5"};
+#define MAX 50
+int countVowel(char []);
+char allVowel[5][1] = {"A", "E", "I", "O", "U"};
+int aVowel=0, eVowel=0, iVowel=0, oVowel=0, uVowel=0;
 
-int checkLogin(char *login, char *passwd) {
-    int i;
+void main() {
+    char text[MAX];
+    int cVowel, i, j;
+    printf("Enter text : ");
+    scanf("%s", text);
+    cVowel = countVowel(text);
+    printf("Text : [%s] has %d vowels\n", text, cVowel);
+    
     for(i=0; i<5; i++) {
-        if( !strcmp(login, loginData[i]) && !strcmp(passwd, passwordData[i]))
-            return 1;
+        printf("%c: ", allVowel[i][0]);
+        if(i==0) {
+            printf("%d", aVowel);
+        } else if(i==1) {
+            printf("%d", eVowel);
+        } else if(i==2) {
+            printf("%d", iVowel);
+        } else if(i==3) {
+            printf("%d", oVowel);
+        } else if(i==4) {
+            printf("%d", uVowel);
+        }
+        
+        printf("\n");
     }
-    return 0;
 }
 
-int main() {
-	char login[64];
-	char password[64];
-
-    printf("Enter login : "); gets(login);
-    printf("Enter password : "); gets(password);
-    if( checkLogin(login, password) == 1) {
-        printf("Welcome\n");
-    } else {
-        printf("Incorrect login or password\n");
+int countVowel(char t[]) {
+    int i=0, count=0;
+    while (i<MAX && t[i]!='\0') {
+        if (t[i]=='A' || t[i]=='a' || t[i]=='E' || t[i]=='e' || t[i]=='I' || t[i]=='i' || t[i]=='O' || t[i]=='o' || t[i]=='U' || t[i]=='u') {
+        count++;
+            switch(t[i]) {
+                case 'A':
+                case 'a':
+                    aVowel++;
+                    break;
+                case 'E':
+                case 'e':
+                    eVowel++;
+                    break;
+                case 'I':
+                case 'i':
+                    iVowel++;
+                    break;
+                case 'O':
+                case 'o':
+                    oVowel++;
+                    break;
+                case 'U':
+                case 'u':
+                    uVowel++;
+                    break;
+            }
+        }
+        
+        i++;
     }
+    return(count);
 }
